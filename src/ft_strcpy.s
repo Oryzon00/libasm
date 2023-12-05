81dec47d672fe6a551f6ddd6d43c0227f1227dd5
Adrian <adrian.mael.jung@gmail.com>
2023-12-05 20:37:49 +0100

    add tab of code

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -9,15 +9,15 @@ section .text
 		;rsi = src
 		xor rcx, rcx ; i = 0
 	
-	loop_start:
-		cmp BYTE [rsi + rcx], 0 ; src[i] != '\0'
-		je loop_end
-		mov al, [rsi + rcx] ; dest[i] = src[i]
-		mov [rdi + rcx], al ; mov ne prends pas de memoire a memoire
-		inc rcx ; i++
-		jmp loop_start
-	
-	loop_end:
-		mov BYTE [rdi + rcx], 0 ; dest[i] = '\0'
-		mov rax, rdi
-		ret
+		loop_start:
+			cmp BYTE [rsi + rcx], 0 ; src[i] != '\0'
+			je loop_end
+			mov al, [rsi + rcx] ; dest[i] = src[i]
+			mov [rdi + rcx], al ; mov ne prends pas de memoire a memoire
+			inc rcx ; i++
+			jmp loop_start
+		
+		loop_end:
+			mov BYTE [rdi + rcx], 0 ; dest[i] = '\0'
+			mov rax, rdi
+			ret
