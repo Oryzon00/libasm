3bc002b8cd3c3e146ba2cc36e5b3215962a4f413
Adrian <adrian.mael.jung@gmail.com>
2023-12-06 18:39:15 +0100

    header file done

---
+++ libasm.h
@@ -0,0 +1,16 @@
+#ifndef LIBASM_H
+# define LIBASM_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+ssize_t	ft_read(int fd, void *buf, size_t count);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+size_t	ft_strlen(const char *s);
+char *	ft_strdup(const char *str);
+char *	ft_strcpy(char *dest, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+
+
+#endif
