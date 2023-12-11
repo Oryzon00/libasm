4fa9e523765c1c0cf51943f05c0654e05687fffc
Adrian <adrian.mael.jung@gmail.com>
2023-12-11 17:30:59 +0100

    STRDUP DONE

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -6,22 +6,26 @@ extern malloc
 
 section .text
 	global ft_strdup
-
 	;char *strdup(const char *str);
 	ft_strdup:
 		;rdi = str
 		call ft_strlen ;arg in rdi
 		; --> len is in rax
 		inc rax ; len + 1 pour malloc
-		mov rsi, rdi ; save str for strcpy
+		push rdi ; save str for strcpy
 		mov rdi, rax
 		call malloc wrt ..plt;arg in rdi
 		; --> allocated address in rax
 		cmp rax, 0 ;check if malloc failed
-		je end_function ;jump to return if malloc failed
+		je error_malloc ;jump to return if malloc failed
 		mov rdi, rax ;dest = rdi
+		pop rsi ; in rsi = rdi = str
 		call ft_strcpy ; args in rdi and rsi
 		; modified str in rax
 
 		end_function:
+			ret
+
+		error_malloc:
+			add rsp, 8
 			ret
