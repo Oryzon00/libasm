86c0ce37cb7b2aed3edb3081ae124bdd0584a4fb
Adrian <adrian.mael.jung@gmail.com>
2023-12-11 15:05:34 +0100

    add stdlib for calloc

--- libasm.h
+++ libasm.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 ssize_t	ft_read(int fd, void *buf, size_t count);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
