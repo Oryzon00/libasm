679493adc8b8d2505b7f27087a513120cd5891ca
Adrian <adrian.mael.jung@gmail.com>
2023-12-11 18:42:02 +0100

    includes for tester

--- libasm.h
+++ libasm.h
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 ssize_t	ft_read(int fd, void *buf, size_t count);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
