ed73f0d8e6f1345a49bb88bca0490566c60f35fa
Adrian <adrian.mael.jung@gmail.com>
2023-12-12 20:07:13 +0100

    include errno for test

--- libasm.h
+++ libasm.h
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 ssize_t	ft_read(int fd, void *buf, size_t count);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
